2cb942a9cd99955e4fd6b6c9883e7c326ff3819c
Marcus Rickert <marcus.rickert@web.de>
2021-05-01 23:24:54 +0200

    *   Bump revision to 92

--- little_brother/settings.py
+++ little_brother/settings.py
@@ -27,7 +27,7 @@ settings = {
 
 extended_settings = {
     "display_url": "github.com/marcus67/little_brother",
-    "debian_package_revision": "91",
+    "debian_package_revision": "92",
     "debian_package_architecture": "all",
     "babel_rel_directory": "translations",
 }
