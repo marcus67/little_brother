02161fd91d713509938e73686aed7f9c95207ef8
Marcus Rickert <marcus.rickert@web.de>
2023-12-28 02:15:57 +0100

    Add navigation bar Move logout from status to navigation bar

--- littlebrother-frontend/src/app/app.module.ts
+++ littlebrother-frontend/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { StatusComponent } from './components/status/status.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
 import { AuthService } from './services/auth.service';
 import { FormsModule } from '@angular/forms';
-import { StatusComponent } from './components/status/status.component';
 import { EnsureAuthenticated } from './services/ensure-authenticated.service';
 import { LoginRedirect } from './services/login-redirect.service';
 
@@ -15,7 +18,8 @@ import { LoginRedirect } from './services/login-redirect.service';
   declarations: [
     AppComponent,
     LoginComponent,
-    StatusComponent
+    StatusComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- littlebrother-frontend/src/app/components/status/status.component.ts
+++ littlebrother-frontend/src/app/components/status/status.component.ts
@@ -1,47 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { NavBarComponent } from '../nav-bar/nav-bar.component'
 
 @Component({
   selector: 'status',
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.css']
 })
-export class StatusComponent implements OnInit {
-  isLoggedIn: boolean = false;
-  constructor(private router: Router, private auth: AuthService, ) {}
-  ngOnInit(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.auth.ensureAuthenticated(token)
-      .then((result) => {
-        console.log(result);
-        if (result.status === 'OK') {
-          this.isLoggedIn = true;
-        } else {
-          localStorage.removeItem('token');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-  }
-
-  onLogout(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.auth.logout(token).then((result) => {
-          console.log(result);
-          localStorage.removeItem('token');
-          if (result.status === 'OK') {
-            this.router.navigate(['/login']);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          localStorage.removeItem('token');
-      });
-    }
-  }
+export class StatusComponent {
 }
