f52b0858cd9d7cb2b167b96e038797e8e662b5f2
Marcus Rickert <marcus.rickert@web.de>
2024-04-01 00:02:14 +0200

    ng update  @angular/cli@16

--- littlebrother-frontend/src/app/services/login-redirect.service.ts
+++ littlebrother-frontend/src/app/services/login-redirect.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class LoginRedirect implements CanActivate {
+export class LoginRedirect  {
   constructor(private auth: AuthService, private router: Router) {}
   canActivate(): boolean {
     if (localStorage.getItem('token')) {
