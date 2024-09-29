97cf275eeda462906c204e37f19265856901d78b
Marcus Rickert <marcus.rickert@web.de>
2024-09-29 23:21:15 +0200

    * Add branch map support for SecureCheckPlus

--- setup.py
+++ setup.py
@@ -89,8 +89,8 @@ extended_setup_params = {
         "fb-angular": 'SONAR_PROJECT_KEY_FB_ANGULAR'
     },
     "owasp_check_branch_map": {
-        "main": 'SECURECHECKPLUS_PROJECT_ID',
-        "fb-angular": 'SECURECHECKPLUS_PROJECT_ID_FB_ANGULAR'
+        "main": ('SECURECHECKPLUS_PROJECT_ID', 'SECURECHECKPLUS_API_KEY'),
+        "fb-angular": ('SECURECHECKPLUS_PROJECT_ID_FB_ANGULAR', 'SECURECHECKPLUS_API_KEY_FB_ANGULAR')
     },
 
     # technical name used for e.g. directories, PIP-package, and users
